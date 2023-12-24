c2794d05c2785aa3d32266883842b54382c68c81
k4mpa15 <kampa.kampa@wp.pl>
2023-12-24 14:46:03 +0100

    change of ctk, req file

--- main_root.py
+++ main_root.py
@@ -1,7 +1,7 @@
 import tkinter
 import customtkinter
 
-# from PIL import Image, ImageTk
+#from PIL import Image, ImageTk
 
 
 # canvas = tkinter.Canvas(root_tk, width=900, height=600)
@@ -15,6 +15,7 @@ COLORS = {
     "MAIN_BUTTONS_COLOR": "#1B36CD",
     "BACKGROUND_COLOR": "#BAC0E4",
     "LIGHT_ENTRY_COLOR": "#B7AAD2",
+    "TEXT_GREY_COLOR": "#5A5A5A",
 }
 
 
@@ -27,6 +28,8 @@ def create_option_button(master, text, x, y, wid, command=None):
         bg_color=COLORS["BACKGROUND_COLOR"],
         fg_color=COLORS["BACKGROUND_COLOR"],
         hover_color="#B7AAD2",
+        font = ("Century Gothic", 13),
+        text_color="black"
     ).place(relx=x, rely=y)
 
 
@@ -38,22 +41,26 @@ def create_main_button(master, text, x, y, wid, hei, anchor, command=None):
         text=text,
         corner_radius=10,
         width=wid,
+        font = ("Century Gothic", 14),
         height=hei,
         text_color="#AEC9F2",
         hover_color="#596CD0",
     ).place(relx=x, rely=y, anchor=anchor)
 
 
-def create_label(master, text, x, y, wid, hei, text_font, text_color, bg_color):
+def create_label(master, text, x, y, wid, hei, 
+                 text_font,
+                 text_color, bg_color, fg_color):
     customtkinter.CTkLabel(
         master=master,
         text=text,
         width=wid,
         height=hei,
         corner_radius=8,
-        text_font=text_font,
+        font=text_font,
         bg_color=bg_color,
         text_color=text_color,
+        fg_color=fg_color
     ).place(relx=x, rely=y)
 
 
@@ -62,10 +69,15 @@ def create_entry(master, x, y, wid, hei):
         master=master,
         width=wid,
         height=hei,
+        font = ("Century Gothic", 20),
         corner_radius=10,
         fg_color=COLORS["LIGHT_ENTRY_COLOR"],
         bg_color=COLORS["BACKGROUND_COLOR"],
+        placeholder_text = "Wpisz równanie",
+        placeholder_text_color = COLORS["TEXT_GREY_COLOR"]
+        
     ).place(relx=x, rely=y)
+
 
 
 def main():
@@ -73,13 +85,15 @@ def main():
     root_tk.title("Kalkulator równań")
     root_tk.geometry("900x600")
     root_tk.resizable(True, True)
+    
     root_tk.configure(background=COLORS["BACKGROUND_COLOR"])
 
-    #combobox = customtkinter.CTkComboBox(master=root_tk,
-    #                                 values=["option 1", "option 2"])
-    #combobox.pack(padx=20, pady=10)
-    #combobox.set("option 2")  # set initial value
 
+
+    # combobox = customtkinter.CTkComboBox(master=root_tk,
+    #                                 values=["option 1", "option 2"])
+    # combobox.pack(padx=20, pady=10)
+    # combobox.set("option 2")  # set initial value
 
     create_label(
         root_tk,
@@ -88,9 +102,10 @@ def main():
         0.05,
         300,
         50,
-        ("Century Gothic", 20),
+        ("Century Gothic", 28),
         "black",
         COLORS["BACKGROUND_COLOR"],
+        COLORS["BACKGROUND_COLOR"]
     )
 
     create_option_button(root_tk, "PL EN", 0.8, 0.0, 120)
@@ -98,11 +113,11 @@ def main():
     # img_history = customtkinter.CTkFrame(Image.open("history_icon.png"), size=(26, 26))
     # history_button = customtkinter.CTkButton(master = root_tk, image = img_history)
     # history_button.place(relx = 0.95, rely = 0.0)
-    
+
     # img_camera = customtkinter.CTkFrame(Image.open("camera_icon.jpg"), size=(26, 26))
     # camera_button = customtkinter.CTkButton(master = root_tk, image = img_camera)
     # camera_button.place(relx = 0.95, rely = 0.0)
-    
+
     # img_export = customtkinter.CTkFrame(Image.open("export_icon.jpg"), size=(26, 26))
     # export_button = customtkinter.CTkButton(master = root_tk, image = img)
     # export_button.place(relx = 0.95, rely = 0.0)
@@ -127,15 +142,27 @@ def main():
         0.7,
         500,
         100,
-        None,
-        "grey",
-        COLORS["LIGHT_ENTRY_COLOR"],
+        ("Century Gothic", 20),
+        COLORS["TEXT_GREY_COLOR"],
+        COLORS["BACKGROUND_COLOR"],
+        COLORS["LIGHT_ENTRY_COLOR"]
+        
     )
 
     create_option_button(root_tk, "Materiały pomocnicze", 0.085, 0.9, 180)
 
     create_label(
-        root_tk, "app version 1.0", 0.43, 0.95, 120, 20, None, "grey", COLORS["BACKGROUND_COLOR"])
+        root_tk,
+        "app version 1.0",
+        0.43,
+        0.95,
+        120,
+        20,
+        None,
+        COLORS["TEXT_GREY_COLOR"],
+        COLORS["BACKGROUND_COLOR"],
+        COLORS["BACKGROUND_COLOR"]
+    )
 
     root_tk.mainloop()
 
